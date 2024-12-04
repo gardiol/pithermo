b4bcaab04749204ebac462951d7ac520c5bf209b
Willy Gardiol <willy.gardiol@leonardo.com>
2024-12-04 13:22:58 +0100

    Reworked MQTT stuff again

--- daemon/src/mqtt_interface.cpp
+++ daemon/src/mqtt_interface.cpp
@@ -7,15 +7,12 @@
 using namespace FrameworkLibrary;
 
 MQTT_Interface::MQTT_Interface(Logger *logger, MQTT_callback* callback, const std::string &host, const std::string &username, const std::string &password, int port)
-    : _host( host )
-    , _port( port )
-    , _username( username )
-    , _password( password )
-    , _connected( false )
+    : _connected( false )
     , _looping( false )
     , _data( NULL )
     , _logger( logger )
     , _callback( callback )
+    , _mutex( "MQTT Callbacks Mutex" )
 {
     if ( mosquitto_lib_init() == MOSQ_ERR_SUCCESS )
     {
@@ -23,9 +20,17 @@ MQTT_Interface::MQTT_Interface(Logger *logger, MQTT_callback* callback, const st
         if ( _data != NULL )
         {
             mosquitto_message_callback_set((struct mosquitto *)_data, MQTT_Interface::_message_callback );
+            mosquitto_connect_callback_set((struct mosquitto *)_data, MQTT_Interface::_connect_callback );
+            mosquitto_disconnect_callback_set((struct mosquitto *)_data, MQTT_Interface::_disconnect_callback );
             if ( mosquitto_loop_start(	(struct mosquitto *)_data ) == MOSQ_ERR_SUCCESS )
             {
-                _looping = true;
+                mosquitto_username_pw_set((struct mosquitto *)_data,
+                                          username.c_str(),
+                                          password.c_str() );
+                mosquitto_connect( (struct mosquitto *)_data,
+                                  host.c_str(),
+                                  port,
+                                  60 );
                 _logger->logDebug( "MQTT created & loop started" );
             }
             else
@@ -42,15 +47,11 @@ MQTT_Interface::~MQTT_Interface()
 {
     if ( _data != NULL )
     {
-        if ( _connected )
-        {
+        if ( isConnected() )
             mosquitto_disconnect( (struct mosquitto *)_data );
-        }
-        if ( _looping )
-        {
-            mosquitto_loop_stop( (struct mosquitto *)_data, true );
-        }
+        mosquitto_loop_stop( (struct mosquitto *)_data, true );
         mosquitto_destroy( (struct mosquitto *)_data );
+        _data = NULL;
         _logger->logDebug( "MQTT all closed." );
     }
     mosquitto_lib_cleanup();
@@ -61,20 +62,16 @@ void MQTT_Interface::publish(const std::string &topic, const std::string &data)
 {
     if ( _data != NULL )
     {
-        _check_connection();
-        if ( _connected )
+        if ( isConnected() )
         {
             if ( int ret = mosquitto_publish( (struct mosquitto *)_data,
-                                  NULL,
-                                  topic.c_str(),
-                                  data.length(),
-                                  data.c_str(),
-                                  0,
-                                  true ) != MOSQ_ERR_SUCCESS )
-            {
-                _logger->logDebug( "MQTT publish failed for topic '" + topic + "' [" + FrameworkUtils::tostring( ret ) + "] (reconnecting)" );
-                _connected = false;
-            }
+                                            NULL,
+                                            topic.c_str(),
+                                            data.length(),
+                                            data.c_str(),
+                                            0,
+                                            true ) != MOSQ_ERR_SUCCESS )
+                _logger->logDebug( "MQTT publish failed for topic '" + topic + "' [" + FrameworkUtils::tostring( ret ) + "]" );
         }
         else
             _logger->logDebug( "MQTT publish failed for topic '" + topic + "' (not connected)" );
@@ -83,15 +80,25 @@ void MQTT_Interface::publish(const std::string &topic, const std::string &data)
 
 void MQTT_Interface::subscribe(const std::string &topic)
 {
+    _mutex.lock();
     _subscribed_topics.insert( topic );
-    if ( !_connected )
-        _check_connection();
-    else
+    _mutex.unlock();
+    if ( isConnected() )
         _subscribe_topic( topic );
 }
 
-void MQTT_Interface::_check_connection()
+bool MQTT_Interface::isConnected()
 {
+    bool connected = false;
+    _mutex.lock();
+    connected = _connected;
+    _mutex.unlock();
+    return connected;
+}
+
+/*void MQTT_Interface::_check_connection()
+{
+    _mutex.lock();
     if ( !_connected )
     {
         mosquitto_username_pw_set((struct mosquitto *)_data,
@@ -103,7 +110,6 @@ void MQTT_Interface::_check_connection()
                                     60 );
         if ( ret  == MOSQ_ERR_SUCCESS )
         {
-            _logger->logDebug( "MQTT connected to " + _host + ":" + FrameworkUtils::tostring( _port ) );
             _connected = true;
             for ( std::set<std::string>::const_iterator t = _subscribed_topics.begin();
                  t != _subscribed_topics.end();
@@ -113,10 +119,9 @@ void MQTT_Interface::_check_connection()
                 _subscribe_topic( topic );
             }
         }
-        else
-            _logger->logDebug( "MQTT connect to " + _host + ":" + FrameworkUtils::tostring( _port ) + " failed [" + FrameworkUtils::tostring( ret ) + "]");
     }
-}
+    _mutex.unlock();
+}*/
 
 void MQTT_Interface::_subscribe_topic(const std::string &topic)
 {
@@ -124,8 +129,44 @@ void MQTT_Interface::_subscribe_topic(const std::string &topic)
     {
         if ( int ret = mosquitto_subscribe( (struct mosquitto *)_data, NULL, topic.c_str(), 0 ) != MOSQ_ERR_SUCCESS )
             _logger->logDebug( "MQTT subscribe failed for topic '" + topic + "' [" + FrameworkUtils::tostring( ret ) + "]" );
-	else
+        else
             _logger->logDebug( "MQTT subscribed to topic '" + topic + "'" );
+    }
+}
+
+void MQTT_Interface::_connect_callback(mosquitto *mosq, void *obj, int rc)
+{
+    MQTT_Interface* me = (MQTT_Interface*)obj;
+    if ( rc == 0 )
+    {
+        me->_mutex.lock();
+        me->_connected = true;
+        for ( std::set<std::string>::const_iterator t = me->_subscribed_topics.begin();
+             t != me->_subscribed_topics.end();
+             ++t )
+        {
+            std::string topic = *t;
+            me->_subscribe_topic( topic );
+        }
+        me->_mutex.unlock();
+        me->_logger->logDebug( "MQTT connected." );
+    }
+    else
+        me->_logger->logDebug( "MQTT connect failed (" + FrameworkUtils::tostring(rc) + "'" );
+}
+
+void MQTT_Interface::_disconnect_callback(mosquitto *mosq, void *obj, int rc)
+{
+    MQTT_Interface* me = (MQTT_Interface*)obj;
+    me->_logger->logDebug( "MQTT disconnected." );
+    // Do not reconnect if disconnect was called by us
+    if ( rc != 0 )
+    {
+        me->_mutex.lock();
+        me->_connected = false;
+        me->_mutex.unlock();
+        me->_logger->logDebug( "MQTT reconnection in progress..." );
+        mosquitto_reconnect( (struct mosquitto *)me->_data );
     }
 }
 
@@ -136,8 +177,6 @@ void MQTT_Interface::_message_callback(mosquitto *mosq,
     MQTT_Interface* me = (MQTT_Interface*)userdata;
     std::string payload( (char*)message->payload, message->payloadlen );
     if ( me->_callback != NULL )
-    {
         me->_callback->message_received( message->topic, payload );
-    }
 }
 
--- daemon/src/mqtt_interface.h
+++ daemon/src/mqtt_interface.h
@@ -28,6 +28,8 @@ public:
     void publish( const std::string& topic, const std::string &data );
     void subscribe( const std::string& topic );
 
+    bool isConnected();
+
 private:
     void _check_connection();
     void _subscribe_topic( const std::string& topic );
@@ -47,6 +49,10 @@ private:
 
     std::set<std::string> _subscribed_topics;
 
+    FrameworkLibrary::BaseMutex _mutex;
+
+    static void _connect_callback(struct mosquitto *mosq, void *obj, int rc);
+    static void _disconnect_callback(struct mosquitto *mosq, void *obj, int rc);
     static void _message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message);
 };
 
